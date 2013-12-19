8796c7b3cb6ec40475820746151a8de38040d6ac
aikko <aikko.woo@gmail.com>
2013-12-19 22:53:29 +0800

    ch

--- public/include/pages/password/change.inc.php
+++ public/include/pages/password/change.inc.php
@@ -6,7 +6,7 @@ if (!defined('SECURITY'))
 
 if (isset($_POST['do']) && $_POST['do'] == 'resetPassword') {
   if ($user->resetPassword($_POST['token'], $_POST['newPassword'], $_POST['newPassword2'])) {
-     $_SESSION['POPUP'][] = array('CONTENT' => '密码重置失败，请重新登录.');
+     $_SESSION['POPUP'][] = array('CONTENT' => '密码重置成功，请重新登录.');
   } else {
     $_SESSION['POPUP'][] = array('CONTENT' => $user->getError(), 'TYPE' => 'errormsg');
   }
